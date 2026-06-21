6bf2657bcc418563ee7a75120c01cbb3f831a27b
NicholasKibiwot <nicolous.kibiwot@gmail.com>
2026-06-21 10:54:42 +0300

    Initial commit

--- app/staffApp/src/main/kotlin/com/track/staff/StaffApplication.kt
+++ app/staffApp/src/main/kotlin/com/track/staff/StaffApplication.kt
@@ -1,7 +1,13 @@
 package com.track.staff
 
-import com.track.common.base.TrackApplication
+import android.app.Application
+import com.google.firebase.FirebaseApp
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class StaffApplication : TrackApplication()
+class StaffApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
